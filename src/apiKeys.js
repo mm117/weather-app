3e85f6a24a94aff297a247094fba348bac837928
MishraMukesh117 <Mukesh.Mishra@accoliteindia.com>
2020-05-19 21:31:06 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "29f793b9447c2f0c7c5642554b670694",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
